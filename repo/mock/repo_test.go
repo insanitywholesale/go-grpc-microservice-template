3f8e4396ce9726ecfd3e2bd8605015b52fa80772
lhz <pepitsizous@zoho.com>
2022-02-24 01:38:03 +0200

    add test for mock repo

---
+++ repo/mock/repo_test.go
@@ -0,0 +1,22 @@
+package mock
+
+import (
+	pb "gitlab.com/insanitywholesale/go-grpc-microservice-template/proto/v1"
+	"testing"
+)
+
+func TestNewMockRepo(t *testing.T) {
+	repo, err := NewMockRepo()
+	if err != nil {
+		t.Fatal("Creating new mock repo failed:", err)
+	}
+	t.Log("New mock repo created", repo)
+}
+
+func TestStoreHello(t *testing.T) {
+	repo, _ := NewMockRepo()
+	err := repo.StoreHello(&pb.HelloResponse{HelloWord: "Hello Test World!"})
+	if err != nil {
+		t.Fatal("Storing Hello failed:", err)
+	}
+}
