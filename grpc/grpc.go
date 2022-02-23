3f2f0cd53f8bd799278b2ec68440e8781afe3533
lhz <pepitsizous@zoho.com>
2022-02-23 16:51:57 +0200

    implement SayHello and SayCustomHello

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -1,9 +1,18 @@
 package grpc
 
 import (
+	"context"
 	pb "gitlab.com/insanitywholesale/go-grpc-microservice-template/proto/v1"
 )
 
 type Server struct {
 	pb.UnimplementedHelloServiceServer
+}
+
+func (Server) SayHello(context.Context, *pb.Empty) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{HelloWord: "Hello World!"}, nil
+}
+
+func (Server) SayCustomHello(_ context.Context, hr *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{HelloWord: "Hello " + hr.CustomWord + "!"}, nil
 }
