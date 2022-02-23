85b2a546da43856a3197a302931d6108f325897d
lhz <pepitsizous@zoho.com>
2022-02-23 16:08:41 +0200

    fill in main.go to run grpc server

--- go.mod
+++ go.mod
@@ -3,14 +3,16 @@ module gitlab.com/insanitywholesale/go-grpc-microservice-template
 go 1.17
 
 require (
-	github.com/ghodss/yaml v1.0.0 // indirect
-	github.com/golang/glog v1.0.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.3 // indirect
-	google.golang.org/genproto v0.0.0-20220118154757-00ab72f36ad5 // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
+	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.3
+	google.golang.org/genproto v0.0.0-20220118154757-00ab72f36ad5
+	google.golang.org/grpc v1.43.0
+)
+
+require (
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/text v0.3.5 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,4 +1,51 @@
 package main
 
+import (
+	hellogrpc "gitlab.com/insanitywholesale/go-grpc-microservice-template/grpc"
+	pbv1 "gitlab.com/insanitywholesale/go-grpc-microservice-template/proto/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"os"
+)
+
+// Global variable for gRPC server port
+var grpcPort string
+
+// Function to determine what port the gRPC server will use
+func setupPort() {
+	// Get value environment variable
+	grpcPort = os.Getenv("HELLO_GRPC_PORT")
+	// If empty, default to 15200
+	if grpcPort == "" {
+		grpcPort = "15200"
+	}
+}
+
+func startGRPCServer() {
+	// Create listener on gRPC port
+	listener, err := net.Listen("tcp", ":"+grpcPort)
+	if err != nil {
+		log.Fatalf("listen failed %v", err)
+	}
+
+	// Create gRPC server
+	grpcServer := grpc.NewServer()
+	// Register v1 HelloService gRPC server
+	pbv1.RegisterHelloServiceServer(grpcServer, hellogrpc.Server{})
+	// Enable reflection so the API can be discoverable by something like grpcurl
+	reflection.Register(grpcServer)
+	// Log the server starting as well as the port it is listening on
+	log.Println("gRPC started on port:", grpcPort)
+	// Start the gRPC server
+	// If an error is returned, log the error and exit fatally
+	log.Fatal(grpcServer.Serve(listener))
+}
+
 func main() {
+	// Set up the port the grpc will listen on
+	setupPort()
+	// Start the gRPC server
+	startGRPCServer()
 }
