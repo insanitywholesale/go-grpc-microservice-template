6d4004239176b48a518f1677221276300f8733a4
lhz <pepitsizous@zoho.com>
2022-02-24 20:07:30 +0200

    add utils tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -18,3 +18,23 @@ func TestSetupPorts(t *testing.T) {
 		t.Fatal("Problem setting up REST port")
 	}
 }
+
+func TestListenerFromPort(t *testing.T) {
+	l, err := ListenerFromPort("1984")
+	if err != nil {
+		t.Fatal("Problem creating listener from port:", err)
+	}
+	t.Log("Listener created:", l)
+}
+
+func TestPortFromListener(t *testing.T) {
+	// TODO: skip for now due to problem
+	t.Skip()
+	// TODO: fix this, causes panic if the check for nil in PortFromListener is not present
+	l, _ := ListenerFromPort("1984")
+	port, err := PortFromListener(l)
+	if err != nil {
+		t.Fatal("Problem extracting port from listener:", err)
+	}
+	t.Log("Port extracted:", port)
+}
