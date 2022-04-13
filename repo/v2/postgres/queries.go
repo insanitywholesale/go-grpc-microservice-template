283a3daf76563f96b1411919d9b8860d50bd0aed
insanitywholesale <popomastor@gmail.com>
2022-04-13 09:59:42 +0300

    revert postgres v2 changes; response shape didn't change and we only store that

--- repo/v2/postgres/queries.go
+++ repo/v2/postgres/queries.go
@@ -3,12 +3,10 @@ package postgres
 var createHellosTableQuery = `
 CREATE TABLE if not exists hellos (
 	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	hello_word VARCHAR,
-	exclamation_point BOOLEAN
+	hello_word VARCHAR
 );`
 
 var helloInsertQuery = `
 INSERT INTO hellos (
-	hello_word,
-	exclamation_point
+	hello_word
 ) VALUES ($1) RETURNING id;`
